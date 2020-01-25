41e80b3b8107382c59fb0f8c36a1e95d34e18278
Luis Egea <jshanks1001@gmail.com>
2020-01-25 09:50:44 -0800

    added greeting.sol

---
+++ greeting.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.0;
+
+contract Greeter{
+  string public greeting;
+
+  constructor() public {
+    greeting = "Hello World!";
+  }
+
+  function greet() public view returns (string memory) {
+    return greeting;
+  }
+
+  function setGreeting(string memory _greeting) public returns (bool) {
+    greeting = _greeting;
+    return true;
+  }
+}
